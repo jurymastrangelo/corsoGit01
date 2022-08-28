a2b1dc502fb010f6ba2235749fae4a3b301e5737
jurymastrangelo <jury.inter2@gmail.com>
2022-08-28 21:08:49 +0200

    Aggiunta funzione moltiplicazione

--- function.php
+++ function.php
@@ -6,10 +6,15 @@ function addizione (n1, n2) {
 return n1+n2;
 }
 
-
 /*sottrazione*/
 
 function sottrazione (n1, n2) {
 return n1-n2;
+}
+
+/*moltiplicazione*/
+
+function moltiplicazione (n1, n2) {
+return n1*n2;
 }
 ?>
